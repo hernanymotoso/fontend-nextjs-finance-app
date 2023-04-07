abc1c282d4ac6eb7187254f7d3a57430900eec15
Hernany Motoso <contatohernany@gmail.com>
2023-04-07 14:58:14 -0300

    feat(app): add keycloak provider and set cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,37 @@
 import "../styles/globals.css";
 import theme from "../utils/theme";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
-import type { AppProps } from "next/app";
+import type { AppContext, AppProps } from "next/app";
 import { useEffect } from "react";
+import { SSRKeycloakProvider, SSRCookies } from "@react-keycloak/ssr";
+import { KEYCLOAK_PUBLIC_CONFIG } from "@/utils/auth";
+import { parseCookies } from "@/utils/cookies";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({
+  Component,
+  pageProps,
+  cookies,
+}: AppProps & { cookies: any }) {
   useEffect(() => {
     const jssStyles = document.querySelector("#jss-server-side");
     jssStyles?.parentElement?.removeChild(jssStyles);
   }, []);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SSRKeycloakProvider
+      keycloakConfig={KEYCLOAK_PUBLIC_CONFIG}
+      persistor={SSRCookies(cookies)}
+    >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SSRKeycloakProvider>
   );
 }
+
+App.getInitialProps = async (appContext: AppContext) => {
+  return {
+    cookies: parseCookies(appContext.ctx.req),
+  };
+};
