b5f22796a7f2c77b95919da01f7f92e86f6bb776
Hernany Motoso <contatohernany@gmail.com>
2023-04-07 17:35:35 -0300

    feat(transactions-page): consume transactions from backend using `getServerSideProps`

--- src/pages/transactions.tsx
+++ src/pages/transactions.tsx
@@ -1,3 +1,5 @@
+import { http } from "../utils/http";
+import { Token, validateAuth } from "../utils/auth";
 import {
   Column,
   IntegratedFiltering,
@@ -15,6 +17,12 @@ import {
   Toolbar,
 } from "@devexpress/dx-react-grid-material-ui";
 import { Container, Typography } from "@material-ui/core";
+import { GetServerSideProps, NextPage } from "next";
+import { Transaction } from "../utils/model";
+
+interface TransactionPageProps {
+  transactions: Transaction[];
+}
 
 const columns: Column[] = [
   {
@@ -39,14 +47,14 @@ const columns: Column[] = [
   },
 ];
 
-const TransactionsPage = () => {
+const TransactionsPage: NextPage<TransactionPageProps> = ({ transactions }) => {
   return (
     <Container>
       <Typography component="h1" variant="h4">
         Minhas transações
       </Typography>
 
-      <Grid rows={[]} columns={columns}>
+      <Grid rows={transactions} columns={columns}>
         <Table />
         <SortingState
           defaultSorting={[{ columnName: "created_at", direction: "desc" }]}
@@ -65,3 +73,24 @@ const TransactionsPage = () => {
 };
 
 export default TransactionsPage;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const auth = validateAuth(ctx.req);
+  if (!auth) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/login",
+      },
+    };
+  }
+  const token = (auth as Token).token;
+  const { data: transactions } = await http.get("transactions", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return {
+    props: { transactions },
+  };
+};
