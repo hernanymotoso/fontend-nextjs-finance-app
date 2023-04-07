b919d084fcf47105098dad940e9608414366379c
Hernany Motoso <contatohernany@gmail.com>
2023-04-07 17:33:09 -0300

    feat: login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,8 @@ import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import type { AppContext, AppProps } from "next/app";
 import { useEffect } from "react";
 import { SSRKeycloakProvider, SSRCookies } from "@react-keycloak/ssr";
-import { KEYCLOAK_PUBLIC_CONFIG } from "@/utils/auth";
-import { parseCookies } from "@/utils/cookies";
+import { KEYCLOAK_PUBLIC_CONFIG } from "../utils/auth";
+import { parseCookies } from "../utils/cookies";
 
 export default function App({
   Component,
@@ -21,6 +21,13 @@ export default function App({
     <SSRKeycloakProvider
       keycloakConfig={KEYCLOAK_PUBLIC_CONFIG}
       persistor={SSRCookies(cookies)}
+      initOptions={{
+        onLoad: "check-sso",
+        silentCheckSsoRedirectUri:
+          typeof window !== "undefined"
+            ? `${window.location.origin}/silent-check-sso.html`
+            : null,
+      }}
     >
       <ThemeProvider theme={theme}>
         <CssBaseline />
