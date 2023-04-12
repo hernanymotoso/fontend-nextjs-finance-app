662f99a845cd7b6413320dbe592faa1f760b98de
Hernany Motoso <contatohernany@gmail.com>
2023-04-12 13:54:36 -0300

    feat: create logout Page

---
+++ src/pages/logout.tsx
@@ -0,0 +1,33 @@
+import { validateAuth } from "../utils/auth";
+import { useKeycloak } from "@react-keycloak/ssr";
+import { KeycloakInstance } from "keycloak-js";
+import { GetServerSideProps } from "next";
+import { destroyCookie } from "../utils/cookies";
+
+export default function LogoutPage() {
+  const { initialized, keycloak } = useKeycloak<KeycloakInstance>();
+  if (initialized && typeof window !== "undefined") {
+    destroyCookie("kcToken");
+    destroyCookie("kcIdToken");
+    keycloak?.logout({
+      redirectUri: window.location.origin + "/login",
+    });
+  }
+  return null;
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx: any) => {
+  const auth = validateAuth(ctx.req);
+  if (!auth) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "login",
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
